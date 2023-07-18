12388e57e9e3c8919ee941e30ba8e9a6b4b9a783
Moniets Semen Petrovich <zevs19932@gmail.com>
2023-07-18 21:23:40 +0300

    Change: articles

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "welcome#index"
+  root "articles#index"
 
   get 'about', to: 'welcome#about'
-  get 'news', to: 'articles#news'
-  get 'cooking', to: 'articles#cooking'
-  get 'roasting', to: 'articles#roasting'
-  get 'other', to: 'articles#other'
+  get 'news', to: 'articles#index'
+  get 'cooking', to: 'articles#index'
+  get 'roasting', to: 'articles#index'
+  get 'other', to: 'articles#index'
 end
